ae32c43af390a82e12c0b94e619f2c698e26ce54
Jaime D <134320299+jdoerr13@users.noreply.github.com>
2024-01-12 13:20:22 -0500

    Eightball upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import EightBall from './EightBall';
+import "./EightBall.css"
+
+function App() {
+  return (
+    <div className="App">
+      <EightBall  />
+    </div>
+  );
+}
+
+export default App;
